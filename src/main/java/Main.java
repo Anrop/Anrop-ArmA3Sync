0b4b572b17b583f2560594d25e68ba9f60ec89ef
Björn Dahlgren <bjorn@dahlgren.at>
2016-05-07 17:37:07 +0200

    Simple Spark API with reading repositories

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,25 @@
-import fr.soe.a3s.main.*;
+import static spark.Spark.*;
+
+import com.google.gson.Gson;
+import fr.soe.a3s.exception.LoadingException;
+import fr.soe.a3s.service.RepositoryService;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(Version.getVersion());
+        RepositoryService repositoryService = new RepositoryService();
+        try {
+            repositoryService.readAll();
+        } catch (LoadingException e) {
+            e.printStackTrace();
+
+            System.err.println("Failed to start");
+            System.exit(1);
+        }
+
+        Gson gson = new Gson();
+
+        get("/repositories", (req, res) -> repositoryService.getRepositories(), gson::toJson);
+        get("/repositories/:name", (req, res) -> repositoryService.getRepository(req.params("name")), gson::toJson);
     }
 }
 

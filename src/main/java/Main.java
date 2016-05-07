0279608c36975a08406e475356f7f42fbb773789
Björn Dahlgren <bjorn@dahlgren.at>
2016-05-07 18:14:34 +0200

    Build action

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,18 @@
 import static spark.Spark.*;
 
 import com.google.gson.Gson;
+import fr.soe.a3s.controller.ObserverText;
 import fr.soe.a3s.exception.LoadingException;
+import fr.soe.a3s.exception.repository.RepositoryException;
 import fr.soe.a3s.service.RepositoryService;
 
+import java.io.IOException;
+
 public class Main {
+    private static RepositoryService repositoryService;
+
     public static void main(String[] args) {
-        RepositoryService repositoryService = new RepositoryService();
+        repositoryService = new RepositoryService();
         try {
             repositoryService.readAll();
         } catch (LoadingException e) {
@@ -16,11 +22,32 @@ public class Main {
             System.exit(1);
         }
 
+        repositoryService.getRepositoryBuilderDAO().addObserverText(new ObserverText() {
+            @Override
+            public void update(String s) {
+                System.out.println(s);
+            }
+        });
+
         Gson gson = new Gson();
 
         get("/repositories", (req, res) -> repositoryService.getRepositories(), gson::toJson);
         get("/repositories/:name", (req, res) -> repositoryService.getRepository(req.params("name")), gson::toJson);
+        post("/repositories/:name/build", (req, res) -> buildRepository(req.params("name")), gson::toJson);
         get("/repositories/:name/events", (req, res) -> repositoryService.getEvents(req.params("name")), gson::toJson);
+    }
+
+    private static Object buildRepository(String name) {
+        try {
+            repositoryService.buildRepository(name);
+            return true;
+        } catch (RepositoryException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return false;
     }
 }
 
